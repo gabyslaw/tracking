0353c73042f09a40254a3db6a2d0779e7ce3922a
AYOKUNLE OLOWONIWA <AYOKUNLE.OLOWONIWA@sterling.ng>
2022-09-24 20:03:23 +0100

    tracking project

---
+++ trackingApi/Data/IssueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using trackingApi.Models;
+
+namespace trackingApi.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
